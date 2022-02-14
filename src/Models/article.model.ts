4c7321320128ecd0dd2a2337b2410a325a852bc1
maroua <marouachoura20@gmail.com>
2022-02-14 09:19:40 +0100

    first commit

---
+++ src/Models/article.model.ts
@@ -0,0 +1,9 @@
+export interface Publications {
+    id: string;
+    nomFormation: string;
+    nomFormateur: string;
+    niveau: string;
+    certification: string;
+    duree: string;
+
+}
---
+++ src/Services/publications.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Publications } from 'src/Models/article.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PublicationsService {
+
+  public tab1: Publications[] = [];
+  constructor(private httpClient: HttpClient) { }
+  savePub(pub: Publications): Promise<Publications> {
+    return this.httpClient.post<Publications>('http://localhost:8080/formations/add', pub).toPromise();
+
+  }
+  affecter(idPub:any,idmembre: any): Promise<void> {
+    return this.httpClient.get<void>('http://localhost:9000/MEMBRE-SERVICE/affecterAuteurToPub/' + idPub +'/' +idmembre).toPromise();
+
+    /*this.tab=this.tab.filter(item => item.id!=id);
+    return new Promise(resolve => resolve());*/
+
+  }
+
+  getPubById(id: string): Promise<Publications> {
+    return this.httpClient.get<Publications>('http://localhost:8080/formations/find/' + id).toPromise();
+    /*return new Promise(resolve => resolve(
+      this.tab.filter(item => item.id===id)[0]??null));*/
+  }
+
+  RemovePubById(id: string): Promise<void> {
+    return this.httpClient.delete<void>('http://localhost:8080/formations/delete/' + id).toPromise();
+
+    /*this.tab=this.tab.filter(item => item.id!=id);
+    return new Promise(resolve => resolve());*/
+
+  }
+
+  /*GetALLMembers(): Promise<Member[]> {
+    //this.httpClient.get<Member>('linkToRestAPI',member).toPromise();
+    return new Promise(resolve => resolve(this.tab));
+
+  }*/
+  GetALL(): Promise<Publications[]> {
+    return this.httpClient.get<any[]>('http://localhost:8080/formations').toPromise();
+
+  }
+  EditPub(id: any, pub: Publications): Promise<Publications> {
+    return this.httpClient.put<Publications>('http://localhost:8080/formation/update/' + id, pub).toPromise();
+
+
+  }
+
+}
