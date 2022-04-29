164566da8e26a09af7ca6c32336694668bdba71e
marouachoura <marouachoura20@gmail.com>
2022-04-29 14:36:12 +0200

    changement dans service publication

--- src/Services/publications.service.ts
+++ src/Services/publications.service.ts
@@ -10,37 +10,32 @@ export class PublicationsService {
   public tab1: Publications[] = [];
   constructor(private httpClient: HttpClient) { }
   savePub(pub: Publications): Promise<Publications> {
-    return this.httpClient.post<Publications>('http://10.66.13.85:32000/api/test/formations/add', pub).toPromise();
+    return this.httpClient.post<Publications>('http://10.66.13.85:8080/api/test/formations/add', pub).toPromise();
 
   }
-    /* test test testt testtt testtttt testttttttt jenkins
-    
+ 
 
   getPubById(id: string): Promise<Publications> {
-    return this.httpClient.get<Publications>('http://10.66.13.85:32000/api/test/formations/find/' + id).toPromise();
+    return this.httpClient.get<Publications>('http://10.66.13.85:8080/api/test/formations/find/' + id).toPromise();
     /*return new Promise(resolve => resolve(
       this.tab.filter(item => item.id===id)[0]??null));*/
   }
 
   RemovePubById(id: string): Promise<void> {
-    return this.httpClient.delete<void>('http://10.66.13.85:32000/api/test/formations/delete/' + id).toPromise();
+    return this.httpClient.delete<void>('http://10.66.13.85:8080/api/test/formations/delete/' + id).toPromise();
 
     /*this.tab=this.tab.filter(item => item.id!=id);
     return new Promise(resolve => resolve());*/
 
   }
 
-  /*GetALLMembers(): Promise<Member[]> {
-    //this.httpClient.get<Member>('linkToRestAPI',member).toPromise();
-    return new Promise(resolve => resolve(this.tab));
 
-  }*/
   GetALL(): Promise<Publications[]> {
-    return this.httpClient.get<any[]>('http://10.66.13.85:32000/api/test/formations').toPromise();
+    return this.httpClient.get<any[]>('http://10.66.13.85:8080/api/test/formations').toPromise();
 
   }
-  EditPub(idpub: any, pub: Publications): Promise<Publications> {
-    return this.httpClient.put<Publications>('http://10.66.13.85:32000/api/test/formation/update/' + idpub, pub).toPromise();
+  EditPub(id: any, pub: Publications): Promise<Publications> {
+    return this.httpClient.put<Publications>('http://10.66.13.85:8080/api/test/formation/update/' + id, pub).toPromise();
 
 
   }
