a2ebd587a19c07506cba0ccda18724784cc6a5b7
marouachoura <marouachoura20@gmail.com>
2022-05-06 10:43:21 +0200

    changement port

--- src/Services/publications.service.ts
+++ src/Services/publications.service.ts
@@ -10,19 +10,19 @@ export class PublicationsService {
   public tab1: Publications[] = [];
   constructor(private httpClient: HttpClient) { }
   savePub(pub: Publications): Promise<Publications> {
-    return this.httpClient.post<Publications>('http://10.66.13.85:8080/api/test/formations/add', pub).toPromise();
+    return this.httpClient.post<Publications>('http://10.66.13.85:32000/api/test/formations/add', pub).toPromise();
 
   }
  
 
   getPubById(id: string): Promise<Publications> {
-    return this.httpClient.get<Publications>('http://10.66.13.85:8080/api/test/formations/find/' + id).toPromise();
+    return this.httpClient.get<Publications>('http://10.66.13.85:32000/api/test/formations/find/' + id).toPromise();
     /*return new Promise(resolve => resolve(
       this.tab.filter(item => item.id===id)[0]??null));*/
   }
 
   RemovePubById(id: string): Promise<void> {
-    return this.httpClient.delete<void>('http://10.66.13.85:8080/api/test/formations/delete/' + id).toPromise();
+    return this.httpClient.delete<void>('http://10.66.13.85:32000/api/test/formations/delete/' + id).toPromise();
 
     /*this.tab=this.tab.filter(item => item.id!=id);
     return new Promise(resolve => resolve());*/
@@ -31,11 +31,11 @@ export class PublicationsService {
 
 
   GetALL(): Promise<Publications[]> {
-    return this.httpClient.get<any[]>('http://10.66.13.85:8080/api/test/formations').toPromise();
+    return this.httpClient.get<any[]>('http://10.66.13.85:32000/api/test/formations').toPromise();
 
   }
   EditPub(id: any, pub: Publications): Promise<Publications> {
-    return this.httpClient.put<Publications>('http://10.66.13.85:8080/api/test/formation/update/' + id, pub).toPromise();
+    return this.httpClient.put<Publications>('http://10.66.13.85:32000/api/test/formation/update/' + id, pub).toPromise();
 
 
   }
