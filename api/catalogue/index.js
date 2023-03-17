a4fbb7b7a69f115c199a924394b6760ba8b32234
Maroua CHOURA <maroua.choura@altersis.com>
2023-03-17 11:19:55 +0000

    Update index.js

--- api/catalogue/index.js
+++ api/catalogue/index.js
@@ -8,14 +8,16 @@
     , app       = express()
 
   app.get("/catalogue/images*", function (req, res, next) {
-    var url = "http://10.66.13.85:8082" + req.url.toString();
+    var url = endpoints.catalogueUrl + req.url.toString();
+    //var url = "http://10.66.13.85:8082" + req.url.toString();
     request.get(url)
         .on('error', function(e) { next(e); })
         .pipe(res);
   });
 
   app.get("/catalogue*", function (req, res, next) {
-    helpers.simpleHttpRequest("http://10.66.13.85:8082" + req.url.toString(), res, next);
+    //helpers.simpleHttpRequest("http://10.66.13.85:8082" + req.url.toString(), res, next);
+    helpers.simpleHttpRequest(endpoints.catalogueUrl + req.url.toString(), res, next);
   });
 
   app.get("/tags", function(req, res, next) {
